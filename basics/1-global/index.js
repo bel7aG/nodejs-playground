08851986b0ff53d3fdf953aaf6969fda822bdeee
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-12 15:52:37 +0100

    init commit ..

---
+++ basics/1-global/index.js
@@ -0,0 +1,5 @@
+console.log(global);
+//  the global is the window of Node unlike the browser he had a window Object.
+
+console.log(process);
+//  the process is like document object in the browser.
