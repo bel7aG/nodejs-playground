45fe3f9a6bf7c1b1b456390bd6cb00af29be0faa
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-18 13:05:45 +0100

    Debugging basics

---
+++ basics/Debugging/uslessWorldApplication.js
@@ -0,0 +1,23 @@
+                                            /*
+
+                                        command Line
+                                             is
+                                   node inspect fileName.js
+                                         n ==> next
+                                             c
+                                            repl
+                                                        
+                                            */
+
+let bel7aG = {
+  name: "lola"
+}
+
+console.log(bel7aG)
+
+
+bel7aG.age = 22
+
+bel7aG.name = "Belhassen Gharsallah"
+
+console.log(bel7aG)
