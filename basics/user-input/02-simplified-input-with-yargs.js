f8ebf5d8c9a1d788d3f9d7330cd368e1ff0f5733
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-20 17:42:04 +0100

    advanced yarg command

--- basics/user-input/02-simplified-input-with-yargs.js
+++ basics/user-input/02-simplified-input-with-yargs.js
@@ -2,7 +2,20 @@ const yargs = require('yargs')
 
 const notes = require('./02-notes.js')
 
-const argv = yargs.argv
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: {
+      describe: 'Title of note',
+      demand: true, /*damand is false by default
+                      Now with demand when you execute:
+                          node file.js add
+                          => Add a new note print out
+                                                          */
+      alias: 't' // now instead of --title i can use -t
+    }
+  })
+  .help() // when i add the --help flag it will
+  .argv
 console.log(argv)
 
 const command = process.argv[2]
