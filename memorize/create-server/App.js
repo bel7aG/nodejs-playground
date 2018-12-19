492f44a3860625c7aca6986c6f41fadd33a974c1
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-19 11:41:41 +0100

    using switch cases for create basic server with pure node not including express

--- memorize/create-server/App.js
+++ memorize/create-server/App.js
@@ -1,8 +1,48 @@
 const http = require(`http`)
+const structure = require(`./structure.js`)
 
 // createServer Method return a Server
 const server = http.createServer((request, response) => {
-  console.log(request)
+  const { url, method, headers } = request
+  // console.log(url, "\n", method, "\n", headers)
+  // the event loop Always running in nodejs that's why our server not exit after execution so with precess.exit() we can force it to exit
+  // process.exit()
+  switch (url) {
+    case '/':
+      const homePage = `
+        <h1>Home Page</h1>
+        <a href="/login">Login</a>
+      `
+      response.write(structure('home', homePage))
+      return response.end()
+
+    break
+
+    case '/login':
+      const loginContent = `
+      <h1>Belhassen Gharsallah</h1>
+      <form action="/ohlala" method="POST">
+        <input type="text" name="name" />
+        <input type="text" name="surname" />
+        <input type="text" name="email" />
+        <button type="submit">Done</button>
+        <a href="/">Home</a>
+      </form>
+      `
+      response.write(structure('login', loginContent))
+      return response.end()
+    break
+    default:
+      const notFound = `4O4`
+      response.write(structure('Not Found', notFound))
+      return response.end()
+  }
+
+
+
+  response.setHeader(`Content-Type`, `text/html`)
+
+
 })
 
 server.listen(5000)
---
+++ memorize/create-server/structure.js
@@ -0,0 +1,13 @@
+const structure = (title = ``, content = `` /*DONT REPEAT YOURSELF*/) => (`
+  <html>
+    <head>
+      <title>${title}</title>
+    </head>
+    <body>
+      <h1>bel7aG</h1>
+      ${content}
+    </body>
+  </html>
+`)
+
+module.exports = structure
