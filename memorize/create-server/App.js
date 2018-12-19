c95bc91f6f962ec972840e973eafbffafc6aafe0
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-19 17:01:58 +0100

    remember creating a sever in nodejs after a hard work in my classroom i miss this node ;D

--- memorize/create-server/App.js
+++ memorize/create-server/App.js
@@ -1,5 +1,7 @@
 const http = require(`http`)
 const structure = require(`./structure.js`)
+const fs = require(`fs`)
+
 
 // createServer Method return a Server
 const server = http.createServer((request, response) => {
@@ -16,12 +18,10 @@ const server = http.createServer((request, response) => {
       response.write(structure('home', homePage))
       return response.end()
 
-    break
-
     case '/login':
       const loginContent = `
       <h1>Belhassen Gharsallah</h1>
-      <form action="/ohlala" method="POST">
+      <form action="/message" method="POST">
         <input type="text" name="name" />
         <input type="text" name="surname" />
         <input type="text" name="email" />
@@ -31,7 +31,31 @@ const server = http.createServer((request, response) => {
       `
       response.write(structure('login', loginContent))
       return response.end()
-    break
+
+    case '/message' :
+      if (method === 'POST') {
+        //the data event will be fired when ever a new chunk is ready to be ret
+        /* execute a function for every incoming data */
+        const body = []
+
+        request.on('data', (chunk) => {
+          body.push(chunk)
+        })
+
+        request.on('end', () => {
+          const parsedBody = Buffer.concat(body).toString();
+          console.log(parsedBody)
+          const inputsValue = parsedBody.split('&').map((input) => input.split('=')[1])
+          console.log(inputsValue)
+          fs.writeFileSync('message.txt', inputsValue)
+        })
+
+        response.statusCode = 302
+        response.setHeader('location', '/')
+        return response.end()
+
+      }
+
     default:
       const notFound = `4O4`
       response.write(structure('Not Found', notFound))
